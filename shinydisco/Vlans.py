8775bd32c4df16289840834d6bf44aa413004f39
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-12-06 22:21:07 +0100

    feat(vlans): add assign method

--- shinydisco/Vlans.py
+++ shinydisco/Vlans.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
+
+
 class Vlans:
-    pass
+
+    def __init__(self, primaries, secondaries):
+        self.primary_vlans = primaries
+        self.secondary_vlans = secondaries
+
+    def assign(self, *, redundant=False):
+        """
+        Picks the first free vlan available, removing it from available ones.
+        In case of redudancy, secondary vlans are also used.
+        """
+        primary = self.primary_vlans.pop(0)
+        if redundant:
+            self.secondary_vlans.pop(0)
+        return primary
--- tests/unit/Vlans.py
+++ tests/unit/Vlans.py
@@ -1,2 +1,23 @@
 # -*- coding: utf-8 -*-
 from shinydisco.Vlans import Vlans
+
+
+def test_vlans_init():
+    vlans = Vlans(['primary'], ['secondary'])
+    assert vlans.primary_vlans == ['primary']
+    assert vlans.secondary_vlans == ['secondary']
+
+
+def test_vlans_assign():
+    vlans = Vlans([{'id': 0, 'device': 0}], [])
+    vlan = vlans.assign()
+    assert vlan == {'id': 0, 'device': 0}
+    assert vlans.primary_vlans == []
+
+
+def test_vlans_assign_redundant():
+    vlans = Vlans([{'id': 0, 'device': 0}], [{'id': 0, 'device': 0}])
+    vlan = vlans.assign(redundant=True)
+    assert vlan == {'id': 0, 'device': 0}
+    assert vlans.primary_vlans == []
+    assert vlans.secondary_vlans == []
