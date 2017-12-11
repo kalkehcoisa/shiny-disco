2b6f02692c869bcbf98c1f448216721cf7a7222c
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-12-11 18:44:27 +0100

    feat(app): process requests and write output

--- shinydisco/App.py
+++ shinydisco/App.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from .Output import Output
+from .Requests import Requests
+from .Vlans import Vlans
 
 
 class App:
@@ -7,5 +10,12 @@ class App:
     """
 
     @staticmethod
-    def run(requests, vlans):
-        pass
+    def run(vlans_file, requests_file, output_file):
+        vlans = Vlans(vlans_file)
+        requests = Requests(requests_file)
+        output = Output(output_file)
+
+        for request in requests.get():
+            vlan = vlans.book(redundant=request['redundant'])
+            output.write(request, vlan)
+        output.save()
--- tests/unit/App.py
+++ tests/unit/App.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from shinydisco.App import App
+from shinydisco.Interface import Interface
+from shinydisco.Output import Output
+from shinydisco.Requests import Requests
+from shinydisco.Vlans import Vlans
 
 
-def test_app_run():
-    assert App.run('requests.csv', 'vlans.csv') is None
+def test_app_run(mocker):
+    mocker.patch.object(Interface, 'read')
+    mocker.patch.object(Output, 'write')
+    mocker.patch.object(Output, 'save')
+    mocker.patch.object(Requests, 'get', return_value=[{'redundant': '0'}])
+    mocker.patch.object(Vlans, 'book')
+    App.run('vlans.csv', 'requests.csv', 'output.csv')
+    Vlans.book.assert_called_with(redundant='0')
+    Output.write.assert_called_with({'redundant': '0'}, Vlans.book())
+    assert Requests.get.call_count == 1
+    assert Output.save.call_count == 1
